9a1c9c87276735e467b232c195205bed80ab7d1c
Richard Hull <rm_hull@yahoo.co.uk>
2025-10-10 00:53:58 +0100

    Dayamn

--- src/server.spec.ts
+++ src/server.spec.ts
@@ -5,11 +5,6 @@ describe("Server", () => {
   let app: ReturnType<typeof createServer>;
 
   beforeAll(() => {
-    process.env.GITHUB_CLIENT_ID = "test-client-id";
-    process.env.GITHUB_CLIENT_SECRET = "test-client-secret";
-    process.env.ALLOWED_ORIGINS = "http://localhost:3000";
-    process.env.DEFAULT_REDIRECT_URI = "http://localhost:3000/callback";
-
     app = createServer();
   });
 
